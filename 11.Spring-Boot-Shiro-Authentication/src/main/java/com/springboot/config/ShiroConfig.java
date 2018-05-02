54f67b0684fe09d64a760348ff13cf9061774156
mrbird <852252810@qq.com>
2018-05-02 16:02:12 +0800

    Spring Boot Shiro用户认证

---
+++ 11.Spring-Boot-Shiro-Authentication/src/main/java/com/springboot/config/ShiroConfig.java
@@ -0,0 +1,59 @@
+package com.springboot.config;
+
+import java.util.LinkedHashMap;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springboot.shiro.ShiroRealm;
+
+@Configuration
+public class ShiroConfig {
+	
+	@Bean
+	public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+		shiroFilterFactoryBean.setSecurityManager(securityManager);
+		shiroFilterFactoryBean.setLoginUrl("/login");
+		shiroFilterFactoryBean.setSuccessUrl("/index");
+		shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+		
+		LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+		
+		filterChainDefinitionMap.put("/css/**", "anon");
+		filterChainDefinitionMap.put("/js/**", "anon");
+		filterChainDefinitionMap.put("/fonts/**", "anon");
+		filterChainDefinitionMap.put("/img/**", "anon");
+		filterChainDefinitionMap.put("/druid/**", "anon");
+		filterChainDefinitionMap.put("/logout", "logout");
+		filterChainDefinitionMap.put("/", "anon");
+		filterChainDefinitionMap.put("/**", "authc");
+		
+		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+		
+		return shiroFilterFactoryBean;
+	}
+ 
+	@Bean  
+    public SecurityManager securityManager(){  
+       DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
+       securityManager.setRealm(shiroRealm());
+       return securityManager;  
+    }  
+	
+	@Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+	
+	@Bean  
+    public ShiroRealm shiroRealm(){  
+       ShiroRealm shiroRealm = new ShiroRealm();  
+       return shiroRealm;  
+    }  
+	
+}
---
+++ 11.Spring-Boot-Shiro-Authentication/src/main/java/com/springboot/shiro/ShiroRealm.java
@@ -0,0 +1,55 @@
+package com.springboot.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springboot.dao.UserMapper;
+import com.springboot.pojo.User;
+
+public class ShiroRealm extends AuthorizingRealm {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	/**
+	 * 获取用户角色和权限
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
+		return null;
+	}
+
+	/**
+	 * 登录认证
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		String userName = (String) token.getPrincipal();
+		String password = new String((char[]) token.getCredentials());
+
+		System.out.println("用户" + userName + "认证-----ShiroRealm.doGetAuthenticationInfo");
+		User user = userMapper.findByUserName(userName);
+
+		if (user == null) {
+			throw new UnknownAccountException("用户名或密码错误！");
+		}
+		if (!password.equals(user.getPassword())) {
+			throw new IncorrectCredentialsException("用户名或密码错误！");
+		}
+		if (user.getStatus().equals("0")) {
+			throw new LockedAccountException("账号已被锁定,请联系管理员！");
+		}
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+		return info;
+	}
+
+}
