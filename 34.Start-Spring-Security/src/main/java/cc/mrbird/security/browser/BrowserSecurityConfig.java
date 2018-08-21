8283310b770788922e93081d6526d1c26216ab8f
mrbird <852252810@qq.com>
2018-08-21 16:07:34 +0800

    Spring Boot中开启Spring Security

---
+++ 34.Start-Spring-Security/src/main/java/cc/mrbird/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,19 @@
+package cc.mrbird.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin() // 表单登录
+        // http.httpBasic() // HTTP Basic
+                .and()
+                .authorizeRequests() // 授权配置
+                .anyRequest()  // 所有请求
+                .authenticated(); // 都需要认证
+    }
+}
---
+++ 34.Start-Spring-Security/src/main/java/cc/mrbird/web/controller/TestController.java
@@ -0,0 +1,12 @@
+package cc.mrbird.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
