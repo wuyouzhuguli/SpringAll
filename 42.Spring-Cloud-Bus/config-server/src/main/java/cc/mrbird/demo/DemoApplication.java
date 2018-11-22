4e54444315fce45edeafaade78e87ae62b59a329
mrbird <852252810@qq.com>
2018-11-22 15:58:08 +0800

    使用Spring Cloud Bus刷新配置

---
+++ 42.Spring-Cloud-Bus/config-server/src/main/java/cc/mrbird/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package cc.mrbird.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class DemoApplication {
+
+    private String a;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
