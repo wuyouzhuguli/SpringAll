95da1d3dd6603ae8eb03a6c9165a9a6581a2f2fa
MrBird <852252810@qq.com>
2020-03-20 16:47:20 +0800

    Spring Cloud Alibaba Sentinel控制台详解

---
+++ 77.spring-cloud-alibaba-sentinel-dashboard-guide/src/main/java/cc/mrbird/sentinel/SpringCloudAlibabaSentinelFlowControlApplication.java
@@ -0,0 +1,13 @@
+package cc.mrbird.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaSentinelFlowControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaSentinelFlowControlApplication.class, args);
+    }
+
+}
