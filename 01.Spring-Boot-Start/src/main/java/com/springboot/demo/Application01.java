0e9622e795f32a0329f5f7b4f699cdd6cb8faddd
xiaowangzi123 <1103694415@qq.com>
2022-01-27 22:23:45 +0800

    开启springboot

--- 01.Spring-Boot-Start/src/main/java/com/springboot/demo/Application01.java
+++ 01.Spring-Boot-Start/src/main/java/com/springboot/demo/Application01.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class Application01 {
 
 	@RequestMapping("/")
 	String index() {
@@ -15,6 +15,6 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application01.class, args);
 	}
 }
